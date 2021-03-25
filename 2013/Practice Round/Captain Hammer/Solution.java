477be9dc89f0c47a7c0c460066caac20dd6903e7
Kai Wang <charles.wangkai@gmail.com>
2021-03-25 10:38:22 +0800

    Captain Hammer

---
+++ 2013/Practice Round/Captain Hammer/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 1; tc <= T; ++tc) {
+      int V = sc.nextInt();
+      int D = sc.nextInt();
+
+      System.out.println(String.format("Case #%d: %.9f", tc, solve(V, D)));
+    }
+
+    sc.close();
+  }
+
+  static double solve(int V, int D) {
+    return Math.toDegrees(
+        Math.asin(
+            Math.sqrt((1 - Math.sqrt(Math.max(0, 1 - 4 * Math.pow(D * 9.8 / 2 / V / V, 2)))) / 2)));
+  }
+}
