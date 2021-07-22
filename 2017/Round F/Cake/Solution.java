f7a12ec69ba27b34ab044c7de1aed2a48ed18e46
Kai Wang <charles.wangkai@gmail.com>
2021-07-22 11:49:15 +0800

    Cake

---
+++ 2017/Round F/Cake/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 1; tc <= T; ++tc) {
+      int N = sc.nextInt();
+
+      System.out.println(String.format("Case #%d: %d", tc, solve(N)));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int N) {
+    int[] dp = new int[N + 1];
+    Arrays.fill(dp, Integer.MAX_VALUE);
+    dp[0] = 0;
+
+    for (int i = 1; i < dp.length; ++i) {
+      for (int j = 1; j * j <= i; ++j) {
+        dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+      }
+    }
+
+    return dp[N];
+  }
+}
