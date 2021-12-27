1575b4257ce5088e2cdd18b792b75ef7873e127d
Kai Wang <charles.wangkai@gmail.com>
2021-12-27 22:35:11 +0800

    Let Me Count The Ways

--- 2018/Round H/Let Me Count The Ways/Solution.java
+++ 2018/Round H/Let Me Count The Ways/Solution.java
@@ -3,8 +3,16 @@ import java.util.Scanner;
 
 public class Solution {
   static final int MODULUS = 1_000_000_007;
+  static final int N_LIMIT = 100000;
+  static final int M_LIMIT = 100000;
+
+  static int[] factorialMods;
+  static int[] factorialModInvs;
+  static int[] pow2s;
 
   public static void main(String[] args) {
+    precompute();
+
     Scanner sc = new Scanner(System.in);
 
     int T = sc.nextInt();
@@ -18,10 +26,28 @@ public class Solution {
     sc.close();
   }
 
+  static void precompute() {
+    factorialMods = new int[N_LIMIT * 2 + 1];
+    factorialMods[0] = 1;
+    factorialModInvs = new int[N_LIMIT * 2 + 1];
+    factorialModInvs[0] = 1;
+    for (int i = 1; i <= N_LIMIT * 2; ++i) {
+      factorialMods[i] = multiplyMod(factorialMods[i - 1], i);
+      factorialModInvs[i] =
+          BigInteger.valueOf(factorialMods[i]).modInverse(BigInteger.valueOf(MODULUS)).intValue();
+    }
+
+    pow2s = new int[M_LIMIT + 1];
+    pow2s[0] = 1;
+    for (int i = 1; i < pow2s.length; ++i) {
+      pow2s[i] = multiplyMod(pow2s[i - 1], 2);
+    }
+  }
+
   static int solve(int N, int M) {
     int result = 0;
     for (int i = 0; i <= M; ++i) {
-      int delta = multiplyMod(multiplyMod(computeFactorialMod(N * 2 - i), CMod(M, i)), pow2Mod(i));
+      int delta = multiplyMod(multiplyMod(factorialMods[N * 2 - i], CMod(M, i)), pow2s[i]);
 
       if (i % 2 == 0) {
         result = addMod(result, delta);
@@ -33,31 +59,8 @@ public class Solution {
     return result;
   }
 
-  static int computeFactorialMod(int x) {
-    int result = 1;
-    for (int i = 1; i <= x; ++i) {
-      result = multiplyMod(result, i);
-    }
-
-    return result;
-  }
-
   static int CMod(int n, int r) {
-    BigInteger result = BigInteger.ONE;
-    for (int i = 0; i < r; ++i) {
-      result = result.multiply(BigInteger.valueOf(n - i)).divide(BigInteger.valueOf(i + 1));
-    }
-
-    return result.mod(BigInteger.valueOf(MODULUS)).intValue();
-  }
-
-  static int pow2Mod(int exponent) {
-    int result = 1;
-    for (int i = 0; i < exponent; ++i) {
-      result = multiplyMod(result, 2);
-    }
-
-    return result;
+    return multiplyMod(factorialMods[n], multiplyMod(factorialModInvs[r], factorialModInvs[n - r]));
   }
 
   static int addMod(int x, int y) {
