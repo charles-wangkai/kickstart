6c0f31462e154d8cdcdd89d06fa58e961588b1be
Kai Wang <charles.wangkai@gmail.com>
2022-01-19 13:48:08 +0800

    Teach Me

---
+++ 2019/Round F/Teach Me/Solution.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 1; tc <= T; ++tc) {
+      int N = sc.nextInt();
+      int S = sc.nextInt();
+      int[][] A = new int[N][];
+      for (int i = 0; i < A.length; ++i) {
+        int C = sc.nextInt();
+        A[i] = new int[C];
+        for (int j = 0; j < A[i].length; ++j) {
+          A[i][j] = sc.nextInt();
+        }
+      }
+
+      System.out.println(String.format("Case #%d: %d", tc, solve(A, S)));
+    }
+
+    sc.close();
+  }
+
+  static long solve(int[][] A, int S) {
+    for (int[] Ai : A) {
+      Arrays.sort(Ai);
+    }
+
+    Map<String, Integer> keyToCount = new HashMap<>();
+    for (int[] Ai : A) {
+      String key = buildKey(Ai, (1 << Ai.length) - 1);
+      keyToCount.put(key, keyToCount.getOrDefault(key, 0) + 1);
+    }
+
+    return Arrays.stream(A)
+        .mapToInt(
+            Ai ->
+                A.length
+                    - IntStream.range(0, 1 << Ai.length)
+                        .map(mask -> keyToCount.getOrDefault(buildKey(Ai, mask), 0))
+                        .sum())
+        .asLongStream()
+        .sum();
+  }
+
+  static String buildKey(int[] skills, int mask) {
+    return IntStream.range(0, skills.length)
+        .filter(i -> (mask & (1 << i)) != 0)
+        .mapToObj(i -> String.valueOf(skills[i]))
+        .collect(Collectors.joining(","));
+  }
+}
