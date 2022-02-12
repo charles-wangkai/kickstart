59bdab6bcf2fa6747c8b7123ee4695eb993ccb28
Kai Wang <charles.wangkai@gmail.com>
2022-02-12 10:13:11 +0800

    Perfect Subarray

---
+++ 2020/Round C/Perfect Subarray/Solution.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Solution {
+  static final int LIMIT = 10_000_000;
+
+  static int[] squares;
+
+  public static void main(String[] args) {
+    buildSquares();
+
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 1; tc <= T; ++tc) {
+      int N = sc.nextInt();
+      int[] A = new int[N];
+      for (int i = 0; i < A.length; ++i) {
+        A[i] = sc.nextInt();
+      }
+
+      System.out.println(String.format("Case #%d: %d", tc, solve(A)));
+    }
+
+    sc.close();
+  }
+
+  static void buildSquares() {
+    squares =
+        IntStream.iterate(0, i -> i + 1).takeWhile(i -> i * i <= LIMIT).map(i -> i * i).toArray();
+  }
+
+  static long solve(int[] A) {
+    Map<Integer, Integer> lastSumToCount = new HashMap<>();
+    int lastSum = 0;
+    lastSumToCount.put(0, 1);
+    for (int i = A.length - 1; i >= 0; --i) {
+      lastSum += A[i];
+      lastSumToCount.put(lastSum, lastSumToCount.getOrDefault(lastSum, 0) + 1);
+    }
+
+    long result = 0;
+    int total = Arrays.stream(A).sum();
+    int firstSum = 0;
+    for (int i = 0; i < A.length; ++i) {
+      lastSumToCount.put(lastSum, lastSumToCount.get(lastSum) - 1);
+
+      for (int square : squares) {
+        result += lastSumToCount.getOrDefault(total - firstSum - square, 0);
+      }
+
+      firstSum += A[i];
+      lastSum -= A[i];
+    }
+
+    return result;
+  }
+}
