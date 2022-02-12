bb96523e2cb78671173c1fec57af4fda19dda1a0
Kai Wang <charles.wangkai@gmail.com>
2022-02-12 22:05:15 +0800

    Record Breaker

---
+++ 2020/Round D/Record Breaker/Solution.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 1; tc <= T; ++tc) {
+      int N = sc.nextInt();
+      int[] V = new int[N];
+      for (int i = 0; i < V.length; ++i) {
+        V[i] = sc.nextInt();
+      }
+
+      System.out.println(String.format("Case #%d: %d", tc, solve(V)));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] V) {
+    int result = 0;
+    int max = Integer.MIN_VALUE;
+    for (int i = 0; i < V.length; ++i) {
+      if (V[i] > max && (i == V.length - 1 || V[i] > V[i + 1])) {
+        ++result;
+      }
+
+      max = Math.max(max, V[i]);
+    }
+
+    return result;
+  }
+}
