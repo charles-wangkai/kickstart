c7c22594853334dfa575eb1b15c19327b61cb785
Kai Wang <charles.wangkai@gmail.com>
2022-02-18 11:46:29 +0800

    Centauri Prime

---
+++ 2022/Coding Practice with Kick Start Session #1/Centauri Prime/Solution.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Set;
+
+public class Solution {
+  static final Set<Character> VOWELS = Set.of('a', 'e', 'i', 'o', 'u');
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 1; tc <= T; ++tc) {
+      String name = sc.next();
+
+      System.out.println(String.format("Case #%d: %s", tc, solve(name)));
+    }
+
+    sc.close();
+  }
+
+  static String solve(String name) {
+    char last = Character.toLowerCase(name.charAt(name.length() - 1));
+
+    String ruler;
+    if (last == 'y') {
+      ruler = "nobody";
+    } else if (VOWELS.contains(last)) {
+      ruler = "Alice";
+    } else {
+      ruler = "Bob";
+    }
+
+    return String.format("%s is ruled by %s.", name, ruler);
+  }
+}
