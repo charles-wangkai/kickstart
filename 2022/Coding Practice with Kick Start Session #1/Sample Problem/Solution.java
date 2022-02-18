2d0d822cd8ad109d05f66ee9dafbccc720c00c58
Kai Wang <charles.wangkai@gmail.com>
2022-02-18 11:45:39 +0800

    Sample Problem

---
+++ 2022/Coding Practice with Kick Start Session #1/Sample Problem/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 1; tc <= T; ++tc) {
+      int N = sc.nextInt();
+      int M = sc.nextInt();
+      int[] C = new int[N];
+      for (int i = 0; i < C.length; ++i) {
+        C[i] = sc.nextInt();
+      }
+
+      System.out.println(String.format("Case #%d: %d", tc, solve(C, M)));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] C, int M) {
+    return Arrays.stream(C).sum() % M;
+  }
+}
