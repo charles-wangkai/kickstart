91e8e2b154fea8ceb75b7e7584b2ab7bf43aea52
Kai Wang <charles.wangkai@gmail.com>
2022-03-04 22:33:51 +0800

    Retype

---
+++ 2020/Round H/Retype/Solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 1; tc <= T; ++tc) {
+      int N = sc.nextInt();
+      int K = sc.nextInt();
+      int S = sc.nextInt();
+
+      System.out.println(String.format("Case #%d: %d", tc, solve(N, K, S)));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int N, int K, int S) {
+    return K + Math.min(N, K - S + N - S);
+  }
+}
