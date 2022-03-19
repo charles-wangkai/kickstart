65c5669e29bc2d0eb8ec1203198eff60d5b19831
Kai Wang <charles.wangkai@gmail.com>
2022-03-19 16:48:49 +0800

    Binary Operator

--- 2021/Round C/Binary Operator/Solution.java
+++ 2021/Round C/Binary Operator/Solution.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Solution {
-  static final String SEPARATOR = "|";
+  static final String SEPARATOR = "-";
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
