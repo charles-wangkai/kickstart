e2f7822af29a8981932e438df207df2cf89c3b3c
Kai Wang <charles.wangkai@gmail.com>
2022-08-22 18:10:50 +0800

    Coloring Game

---
+++ 2022/Round E/Coloring Game/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 1; tc <= T; ++tc) {
+      int N = sc.nextInt();
+
+      System.out.println(String.format("Case #%d: %d", tc, solve(N)));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int N) {
+    return (N - 1) / 5 + 1;
+  }
+}
