9373ca6e705f65f3a70b2d5315847b31b572ba5a
Kai Wang <charles.wangkai@gmail.com>
2022-11-23 10:53:02 +0800

    Running in Circles

---
+++ 2022/Round H/Running in Circles/Solution.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 1; tc <= T; ++tc) {
+      int L = sc.nextInt();
+      int N = sc.nextInt();
+      int[] D = new int[N];
+      char[] C = new char[N];
+      for (int i = 0; i < N; ++i) {
+        D[i] = sc.nextInt();
+        C[i] = sc.next().charAt(0);
+      }
+
+      System.out.println(String.format("Case #%d: %d", tc, solve(L, D, C)));
+    }
+
+    sc.close();
+  }
+
+  static long solve(int L, int[] D, char[] C) {
+    long result = 0;
+    char direction = 'C';
+    int distance = 0;
+    for (int i = 0; i < D.length; ++i) {
+      if (C[i] == direction) {
+        distance = Math.abs(distance) + D[i];
+
+        result += distance / L;
+        distance %= L;
+      } else {
+        distance = -Math.abs(distance) + D[i];
+
+        if (distance >= 0) {
+          direction = C[i];
+
+          result += distance / L;
+          distance %= L;
+        }
+      }
+    }
+
+    return result;
+  }
+}
