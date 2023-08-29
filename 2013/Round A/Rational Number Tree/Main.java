aa1e042cfcf0ce5941d198df8a9f78ada4c19fa0
Kai Wang <charles.wangkai@gmail.com>
2023-08-29 21:19:39 +0800

    Rational Number Tree

--- 2013/Round A/Rational Number Tree/Main.java
+++ 2013/Round A/Rational Number Tree/Main.java
@@ -1,7 +1,7 @@
 import java.math.BigInteger;
 import java.util.Scanner;
 
-public class Solution {
+public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
