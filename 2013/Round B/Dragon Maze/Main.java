8e780159eefa6498c19a60ff0a3dd7ee532d0895
Kai Wang <charles.wangkai@gmail.com>
2023-08-29 23:33:57 +0800

    Dragon Maze

--- 2013/Round B/Dragon Maze/Main.java
+++ 2013/Round B/Dragon Maze/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Solution {
+public class Main {
   static final int[] R_OFFSETS = {-1, 0, 1, 0};
   static final int[] C_OFFSETS = {0, 1, 0, -1};
 
