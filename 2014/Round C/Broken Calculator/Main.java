b1af92b058db0d11e517bf51de1e5387a704fcd0
Kai Wang <charles.wangkai@gmail.com>
2023-08-30 20:19:54 +0800

    Broken Calculator

--- 2014/Round C/Broken Calculator/Main.java
+++ 2014/Round C/Broken Calculator/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class Solution {
+public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
