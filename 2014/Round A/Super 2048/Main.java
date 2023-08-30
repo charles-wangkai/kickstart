07c13477ff787815ae48bf06a5232587d0537b1e
Kai Wang <charles.wangkai@gmail.com>
2023-08-30 18:21:56 +0800

    Super 2048

--- 2014/Round A/Super 2048/Main.java
+++ 2014/Round A/Super 2048/Main.java
@@ -6,7 +6,7 @@ import java.util.Queue;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class Solution {
+public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
