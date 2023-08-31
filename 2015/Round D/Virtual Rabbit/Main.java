ca607f46c331091dd371502186525aa4622070ca
Kai Wang <charles.wangkai@gmail.com>
2023-08-31 23:32:44 +0800

    Virtual Rabbit

--- 2015/Round D/Virtual Rabbit/Main.java
+++ 2015/Round D/Virtual Rabbit/Main.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Solution {
+public class Main {
   static final int DAY_SECOND_NUM = 86400;
 
   public static void main(String[] args) {
