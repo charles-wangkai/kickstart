fe6f8d2616e0a37f463b8c2db6dbc60030ad8324
Kai Wang <charles.wangkai@gmail.com>
2023-08-31 23:22:29 +0800

    gMatrix

--- 2015/Round C/gMatrix/Main.java
+++ 2015/Round C/gMatrix/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-public class Solution {
+public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
