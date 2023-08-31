c20a6b4f58bfbaa230e28f0c78e112b8a6fe8fab
Kai Wang <charles.wangkai@gmail.com>
2023-08-31 20:21:17 +0800

    gSnake

--- 2015/Round A/gSnake/Main.java
+++ 2015/Round A/gSnake/Main.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 import java.util.Scanner;
 import java.util.Set;
 
-public class Solution {
+public class Main {
   static final int[] R_OFFSETS = {-1, 0, 1, 0};
   static final int[] C_OFFSETS = {0, 1, 0, -1};
 
