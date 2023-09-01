4893f6c4faf1ab6ec1546cf69b436f36e0178e23
Kai Wang <charles.wangkai@gmail.com>
2023-09-01 13:56:07 +0800

    Evaluation

--- 2016/Round C/Evaluation/Main.java
+++ 2016/Round C/Evaluation/Main.java
@@ -9,7 +9,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Solution {
+public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
