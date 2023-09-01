ea83f3396627672ed5a5a04aa896d544924853b3
Kai Wang <charles.wangkai@gmail.com>
2023-09-01 23:52:51 +0800

    Cards Game

--- 2017/Round G/Cards Game/Main.java
+++ 2017/Round G/Cards Game/Main.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
-public class Solution {
+public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
