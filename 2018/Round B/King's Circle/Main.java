40c705f0f538949793eb5dd00b743b9047f6b6a3
Kai Wang <charles.wangkai@gmail.com>
2023-09-02 23:17:37 +0800

    King's Circle

--- 2018/Round B/King's Circle/Main.java
+++ 2018/Round B/King's Circle/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Solution {
+public class Main {
   static final int SIZE = 1 + (1 << 20);
 
   public static void main(String[] args) {
