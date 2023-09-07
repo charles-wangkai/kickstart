0466a6b9f787c8b4c189e3468fd5031162b795b3
Kai Wang <charles.wangkai@gmail.com>
2023-09-07 17:42:59 +0800

    Let Me Count The Ways

--- 2018/Round H/Let Me Count The Ways/Main.java
+++ 2018/Round H/Let Me Count The Ways/Main.java
@@ -1,7 +1,7 @@
 import java.math.BigInteger;
 import java.util.Scanner;
 
-public class Solution {
+public class Main {
   static final int MODULUS = 1_000_000_007;
   static final int N_LIMIT = 100000;
   static final int M_LIMIT = 100000;
