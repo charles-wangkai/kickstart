f9e5f8c5459f49d870a1aec5eaf9d785457019bc
Kai Wang <charles.wangkai@gmail.com>
2023-09-09 23:17:11 +0800

    Catch Some

--- 2019/Round C/Catch Some/Main.java
+++ 2019/Round C/Catch Some/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Solution {
+public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
