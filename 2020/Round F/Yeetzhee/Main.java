f7eb3ddef61e1cbd033e6cab0b5b77c5561b6b00
Kai Wang <charles.wangkai@gmail.com>
2023-09-12 07:41:26 +0800

    Yeetzhee

--- 2020/Round F/Yeetzhee/Main.java
+++ 2020/Round F/Yeetzhee/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Solution {
+public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
