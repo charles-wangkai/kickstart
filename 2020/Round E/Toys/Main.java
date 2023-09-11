3f1c09350fec3e8a0b77f1a6d4ee9076cd0bbffe
Kai Wang <charles.wangkai@gmail.com>
2023-09-11 09:27:33 +0800

    Toys

--- 2020/Round E/Toys/Main.java
+++ 2020/Round E/Toys/Main.java
@@ -3,7 +3,7 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-public class Solution {
+public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
