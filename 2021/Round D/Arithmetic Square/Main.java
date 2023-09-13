4d4affa644f1411c5a7c7572c7002aaaeacbac3d
Kai Wang <charles.wangkai@gmail.com>
2023-09-13 23:05:10 +0800

    Arithmetic Square

--- 2021/Round D/Arithmetic Square/Main.java
+++ 2021/Round D/Arithmetic Square/Main.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Solution {
+public class Main {
   static final int SIZE = 3;
 
   public static void main(String[] args) {
