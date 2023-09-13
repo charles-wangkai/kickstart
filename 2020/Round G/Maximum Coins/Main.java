b5a0c18076279fe0e45aa22d68db203c557459e7
Kai Wang <charles.wangkai@gmail.com>
2023-09-13 15:13:17 +0800

    Maximum Coins

--- 2020/Round G/Maximum Coins/Main.java
+++ 2020/Round G/Maximum Coins/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
-public class Solution {
+public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
