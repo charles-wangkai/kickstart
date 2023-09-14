937cdb08936ce5871686356debfd97ae6f933b33
Kai Wang <charles.wangkai@gmail.com>
2023-09-14 23:30:25 +0800

    Palindromic Factors

--- 2022/Round B/Palindromic Factors/Main.java
+++ 2022/Round B/Palindromic Factors/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Solution {
+public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
