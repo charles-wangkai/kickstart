e60336079a679457662e737b832212cc75a3e999
Kai Wang <charles.wangkai@gmail.com>
2023-09-14 20:58:03 +0800

    Centauri Prime

--- 2022/Coding Practice with Kick Start Session #1/Centauri Prime/Main.java
+++ 2022/Coding Practice with Kick Start Session #1/Centauri Prime/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Set;
 
-public class Solution {
+public class Main {
   static final Set<Character> VOWELS = Set.of('a', 'e', 'i', 'o', 'u');
 
   public static void main(String[] args) {
