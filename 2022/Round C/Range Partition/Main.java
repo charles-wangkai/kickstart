25a5f10ef8cc0ac5c84c12b9b96c78b93d8b0096
Kai Wang <charles.wangkai@gmail.com>
2023-09-14 23:44:01 +0800

    Range Partition

--- 2022/Round C/Range Partition/Main.java
+++ 2022/Round C/Range Partition/Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class Solution {
+public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
